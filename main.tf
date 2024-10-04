aa5a6d12cff1d62ffabd3d93e6302d25a6ba621e
unknown <vvdswamy7479@gmail.com>
2024-10-04 15:26:21 +0530

    terraform-aws-vpc

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 resource "aws_vpc" "main" {
   cidr_block       = var.vpc_cidr
   enable_dns_hostnames = var.enable_dns_hostnames
-  tags = merge(var.common_tags, var.vpc_tags)
+  tags = merge(
+    var.common_tags, 
+    var.vpc_tags,
+    {
+      Name = "${var.project_name}-${var.environment}"
+    }
+  )
 }
