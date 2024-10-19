ca5b5cad59d994f422475b756259059e23c5412a
unknown <vvdswamy7479@gmail.com>
2024-10-19 17:02:34 +0530

    terraform-aws-vpc

--- variables.tf
+++ variables.tf
@@ -83,11 +83,16 @@ variable "database_route_table_tags" {
   default = {}
 }
 
+variable "is_peering_required" {
+  type = bool
+  default = false
+}
+
 variable "acceptor_vpc_id" {
     type = string
     default = ""
 }
 
-variable "vpc_peering_tags {
+variable "vpc_peering_tags" {
     default = {}
 }
